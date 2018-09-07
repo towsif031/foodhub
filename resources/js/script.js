e18bed4da385ba2923743e60b64cdb7025058658
Towsif Ahmed Omi <towsif031@gmail.com>
2018-09-07 12:01:47 +0600

    Fixed navigation scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,71 +1,95 @@
 $(document).ready(function() {
-    
+
     /* Sticky navigation */
     $('.js--section-features').waypoint(function(direction) {
-       if (direction == "down") {
-           $('nav').addClass('sticky');
-       } else {
-           $('nav').removeClass('sticky');
-       }
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
     }, {
         offset: '60px;'
     });
-    
+
     /* Scroll on buttons */
     $('.js--scroll-to-plans').click(function() {
         $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
     })
-    
+
     $('.js--scroll-to-start').click(function() {
         $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
     })
-    
+
     /* Navigation scroll */
     $(function() {
-      $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html, body').animate({
-              scrollTop: target.offset().top
-            }, 1000);
-            return false;
-          }
-        }
-      });
+        // Select all links with hashes
+        $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+            .not('[href="#"]')
+            .not('[href="#0"]')
+            .click(function(event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+                && 
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function() {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
     });
-    
+
     /* Animations on scroll */
     $('.js--wp-1').waypoint(function(direction) {
         $('.js--wp-1').addClass('animated fadeIn');
     }, {
         offset: '50%'
     });
-    
+
     $('.js--wp-2').waypoint(function(direction) {
         $('.js--wp-2').addClass('animated fadeInUp');
     }, {
         offset: '50%'
     });
-    
+
     $('.js--wp-3').waypoint(function(direction) {
         $('.js--wp-3').addClass('animated fadeIn');
     }, {
         offset: '50%'
     });
-    
+
     $('.js--wp-4').waypoint(function(direction) {
         $('.js--wp-4').addClass('animated pulse');
     }, {
         offset: '50%'
     });
-    
+
     /* Mobile nav */
     $('.js--nav-icon').click(function() {
         var nav = $('.js--main-nav');
         var icon = $('.js--nav-icon i');
-        
+
         nav.slideToggle(200);
         if (icon.hasClass('ion-navicon-round')) {
             icon.addClass('ion-close-round');
@@ -75,7 +99,7 @@ $(document).ready(function() {
             icon.removeClass('ion-close-round');
         }
     });  
-    
+
     /* Maps */
     var map = new GMaps({
         div: '.map',
@@ -83,7 +107,7 @@ $(document).ready(function() {
         lng: 90.381,
         zoom: 16
     });
-    
+
     map.addMarker({
         lat: 23.7470261,
         lng: 90.3737121,
@@ -92,10 +116,4 @@ $(document).ready(function() {
             content: '<p>Our Dhanmondi HQ</p>'
         }
     });
-    
-    
-    
-    
-    
-    
 });
